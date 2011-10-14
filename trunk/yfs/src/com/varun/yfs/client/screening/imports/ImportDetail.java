50b768e1dccbd4f920774ebef863123858c11dfc
varun.tayur@gmail.com <varun.tayur@gmail.com@63eca3cb-c5b2-5fb5-8018-6bcedb7b94c6>
2011-10-14 12:18:31 +0000

    Import Page Design layout added

--- trunk/yfs/src/com/varun/yfs/client/screening/imports/ImportDetail.java
+++ trunk/yfs/src/com/varun/yfs/client/screening/imports/ImportDetail.java
@@ -12,10 +12,21 @@ import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.ProgressBar;
 import com.extjs.gxt.ui.client.widget.layout.FitData;
+import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.extjs.gxt.ui.client.widget.layout.FormLayout;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.varun.yfs.client.index.IndexPage;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.Text;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
+import com.extjs.gxt.ui.client.widget.form.LabelField;
+import com.extjs.gxt.ui.client.widget.layout.TableLayout;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.layout.TableData;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.form.Radio;
 
 public class ImportDetail extends LayoutContainer
 {
@@ -30,13 +41,74 @@ public class ImportDetail extends LayoutContainer
 	{
 		super.onRender(parent, index);
 
+		setLayout(new FitLayout());
+
+		ContentPanel mainContainerPanel = new ContentPanel();
+		mainContainerPanel.setHeading("Import Screening Data");
+		mainContainerPanel.setCollapsible(true);
+		add(mainContainerPanel);
+
+		LayoutContainer lcUploadComponent = buildUploadComponent();
+		LayoutContainer lcTemplateComponent = buildTemplateComponent();
+
+		mainContainerPanel.add(lcUploadComponent, new FitData(5));
+		lcTemplateComponent_1.setLayout(new FitLayout());
+		mainContainerPanel.add(lcTemplateComponent, new FitData(5));
+		add(mainContainerPanel);
+	}
+
+	private LayoutContainer buildTemplateComponent()
+	{
+		lcTemplateComponent_1 = new LayoutContainer();
+		
+		ContentPanel mainTemplateSettingsPanel = new ContentPanel();
+		mainTemplateSettingsPanel.setHeading("Template Settings");
+		mainTemplateSettingsPanel.setCollapsible(true);
+		mainTemplateSettingsPanel.setLayout(new TableLayout(2));
+		
+		FormPanel templateSettings1 = new FormPanel();
+		templateSettings1.setLayout(new FormLayout());
+		templateSettings1.setHeaderVisible(false);
+		templateSettings1.setHeading("New FormPanel");
+		templateSettings1.setCollapsible(true);
+		templateSettings1.setWidth("250");
+		
+		TextField txtfldFirstDataRow = new TextField();
+		templateSettings1.add(txtfldFirstDataRow, new FormData("100%"));
+		txtfldFirstDataRow.setFieldLabel("First Data Row");
+		
+		FormPanel templateSettings2 = new FormPanel();
+		templateSettings2.setHeaderVisible(false);
+		templateSettings2.setHeading("New FormPanel");
+		templateSettings2.setCollapsible(true);
+		
+		mainTemplateSettingsPanel.add(templateSettings1);
+		mainTemplateSettingsPanel.add(templateSettings2);
+		mainTemplateSettingsPanel.setHeight("200");
+		
+		lcTemplateComponent_1.add(mainTemplateSettingsPanel);
+		return lcTemplateComponent_1;
+	}
+
+	private LayoutContainer buildUploadComponent()
+	{
+		LayoutContainer lcUploadComponent = new LayoutContainer();
+		lcUploadComponent.setLayout(new TableLayout(2));
+
+		LabelField lblFileImport = new LabelField("Select a file to Import:");
+		lblFileImport.setSize("120", "20");
+
 		MultiUploader defaultUploader = new MultiUploader();
 		defaultUploader.setValidExtensions("xls", "xlsx");
 		defaultUploader.addOnFinishUploadHandler(onFinishUploaderHandler);
 		defaultUploader.setMaximumFiles(1);
 		defaultUploader.setAvoidRepeatFiles(true);
 		defaultUploader.setTitle("Select a file to upload:");
-		add(defaultUploader, new FitData(5));
+
+		lcUploadComponent.add(lblFileImport);
+		lcUploadComponent.add(defaultUploader);
+		lcUploadComponent.setWidth("400");
+		return lcUploadComponent;
 	}
 
 	public void initialize(String title, String scrId)
@@ -61,7 +133,7 @@ public class ImportDetail extends LayoutContainer
 				// You can send any customized message and parse it
 				System.out.println("Server message " + info.message);
 
-				final MessageBox box = MessageBox.progress("Please wait", "Loading items...", "Initializing...");
+				final MessageBox box = MessageBox.progress("Please wait", "Processing records...", "");
 				final ProgressBar bar = box.getProgressBar();
 				final Timer t = new Timer()
 				{
@@ -94,4 +166,5 @@ public class ImportDetail extends LayoutContainer
 			panelImages.add(image);
 		}
 	};
+	private LayoutContainer lcTemplateComponent_1;
 }
