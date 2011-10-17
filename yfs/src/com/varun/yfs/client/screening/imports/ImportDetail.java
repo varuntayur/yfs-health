a6b5e267777750d0fe63a8047fd869cf6c1b1071
varun.tayur@gmail.com <varun.tayur@gmail.com@63eca3cb-c5b2-5fb5-8018-6bcedb7b94c6>
2011-10-17 02:09:08 +0000

    Screening detail import changes

--- yfs/src/com/varun/yfs/client/screening/imports/ImportDetail.java
+++ yfs/src/com/varun/yfs/client/screening/imports/ImportDetail.java
@@ -7,26 +7,26 @@ import gwtupload.client.MultiUploader;
 import gwtupload.client.PreloadedImage;
 import gwtupload.client.PreloadedImage.OnLoadPreloadedImageHandler;
 
+import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Info;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.ProgressBar;
+import com.extjs.gxt.ui.client.widget.form.FieldSet;
+import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.LabelField;
+import com.extjs.gxt.ui.client.widget.form.NumberField;
 import com.extjs.gxt.ui.client.widget.layout.FitData;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
+import com.extjs.gxt.ui.client.widget.layout.FormData;
 import com.extjs.gxt.ui.client.widget.layout.FormLayout;
+import com.extjs.gxt.ui.client.widget.layout.TableLayout;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.varun.yfs.client.index.IndexPage;
-import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Text;
-import com.extjs.gxt.ui.client.widget.layout.FormData;
-import com.extjs.gxt.ui.client.widget.form.LabelField;
-import com.extjs.gxt.ui.client.widget.layout.TableLayout;
-import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.layout.TableData;
-import com.extjs.gxt.ui.client.widget.form.TextField;
-import com.extjs.gxt.ui.client.widget.form.Radio;
 
 public class ImportDetail extends LayoutContainer
 {
@@ -34,6 +34,7 @@ public class ImportDetail extends LayoutContainer
 
 	public ImportDetail()
 	{
+		setSize("600", "575");
 	}
 
 	@Override
@@ -45,8 +46,8 @@ public class ImportDetail extends LayoutContainer
 
 		ContentPanel mainContainerPanel = new ContentPanel();
 		mainContainerPanel.setHeading("Import Screening Data");
-		mainContainerPanel.setCollapsible(true);
 		add(mainContainerPanel);
+		mainContainerPanel.setSize("600px", "");
 
 		LayoutContainer lcUploadComponent = buildUploadComponent();
 		LayoutContainer lcTemplateComponent = buildTemplateComponent();
@@ -54,40 +55,139 @@ public class ImportDetail extends LayoutContainer
 		mainContainerPanel.add(lcUploadComponent, new FitData(5));
 		lcTemplateComponent_1.setLayout(new FitLayout());
 		mainContainerPanel.add(lcTemplateComponent, new FitData(5));
+		lcTemplateComponent_1.setSize("600", "");
 		add(mainContainerPanel);
 	}
 
 	private LayoutContainer buildTemplateComponent()
 	{
 		lcTemplateComponent_1 = new LayoutContainer();
-		
+
 		ContentPanel mainTemplateSettingsPanel = new ContentPanel();
 		mainTemplateSettingsPanel.setHeading("Template Settings");
-		mainTemplateSettingsPanel.setCollapsible(true);
-		mainTemplateSettingsPanel.setLayout(new TableLayout(2));
-		
+		mainTemplateSettingsPanel.setLayout(new TableLayout(3));
+
 		FormPanel templateSettings1 = new FormPanel();
 		templateSettings1.setLayout(new FormLayout());
 		templateSettings1.setHeaderVisible(false);
 		templateSettings1.setHeading("New FormPanel");
-		templateSettings1.setCollapsible(true);
-		templateSettings1.setWidth("250");
-		
-		TextField txtfldFirstDataRow = new TextField();
-		templateSettings1.add(txtfldFirstDataRow, new FormData("100%"));
-		txtfldFirstDataRow.setFieldLabel("First Data Row");
-		
+		templateSettings1.setSize("200", "330");
+
+		NumberField firstDataRow = new NumberField();
+		firstDataRow.setMaxLength(2);
+		templateSettings1.add(firstDataRow, new FormData("100%"));
+		firstDataRow.setFieldLabel("First Data Row");
+
+		NumberField nameColumn = new NumberField();
+		nameColumn.setMaxLength(2);
+		templateSettings1.add(nameColumn, new FormData("100%"));
+		nameColumn.setFieldLabel("Name Column");
+
+		NumberField ageColumn = new NumberField();
+		ageColumn.setMaxLength(2);
+		ageColumn.setFieldLabel("Age Column");
+		templateSettings1.add(ageColumn, new FormData("50%"));
+
+		NumberField sexColumn = new NumberField();
+		sexColumn.setFieldLabel("Sex Column");
+		templateSettings1.add(ageColumn, new FormData("100%"));
+
+		NumberField standardColumn = new NumberField();
+		standardColumn.setMaxLength(2);
+		standardColumn.setFieldLabel("Standard Column");
+		templateSettings1.add(standardColumn, new FormData("100%"));
+
+		NumberField heightColumn = new NumberField();
+		heightColumn.setMaxLength(2);
+		heightColumn.setFieldLabel("Height Column");
+		templateSettings1.add(heightColumn, new FormData("100%"));
+
+		NumberField weightColumn = new NumberField();
+		weightColumn.setMaxLength(2);
+		weightColumn.setFieldLabel("Weight Column");
+		templateSettings1.add(weightColumn, new FormData("100%"));
+
+		NumberField addressColumn = new NumberField();
+		addressColumn.setMaxLength(2);
+		addressColumn.setFieldLabel("Address Column");
+		templateSettings1.add(addressColumn, new FormData("100%"));
+
 		FormPanel templateSettings2 = new FormPanel();
+		templateSettings2.setSize("200", "330");
 		templateSettings2.setHeaderVisible(false);
 		templateSettings2.setHeading("New FormPanel");
 		templateSettings2.setCollapsible(true);
+
+		FieldSet fldstPaediatric = new FieldSet();
+		fldstPaediatric.setLayout(new FormLayout());
+		templateSettings2.add(fldstPaediatric, new FormData("100%"));
+		fldstPaediatric.setHeading("Paediatric ");
+		fldstPaediatric.setCollapsible(true);
+		buildScreeningDetailTemplate(fldstPaediatric);
+
+		FieldSet fldstDental = new FieldSet();
+		fldstDental.setLayout(new FormLayout());
+		templateSettings2.add(fldstDental, new FormData("100%"));
+		fldstDental.setHeading("Dental");
+		fldstDental.setCollapsible(true);
+		buildScreeningDetailTemplate(fldstDental);
+
+		FieldSet fldstEye = new FieldSet();
+		fldstEye.setLayout(new FormLayout());
+		templateSettings2.add(fldstEye, new FormData("100%"));
+		fldstEye.setHeading("Eye");
+		fldstEye.setCollapsible(true);
+		buildScreeningDetailTemplate(fldstEye);
+
+
+		FormPanel templateSettings3 = new FormPanel();
+		templateSettings3.setSize("200", "260");
+		templateSettings3.setHeaderVisible(false);
+		templateSettings3.setHeading("New FormPanel");
+		templateSettings3.setCollapsible(true);
+
+		FieldSet fldstSkin = new FieldSet();
+		templateSettings3.add(fldstSkin, new FormData("100%"));
+		fldstSkin.setLayout(new FormLayout());
+		fldstSkin.setHeading("Skin");
+		fldstSkin.setCollapsible(true);
+		buildScreeningDetailTemplate(fldstSkin);
+
+		FieldSet fldstEnt = new FieldSet();
+		templateSettings3.add(fldstEnt, new FormData("100%"));
+		fldstEnt.setLayout(new FormLayout());
+		fldstEnt.setHeading("ENT");
+		fldstEnt.setCollapsible(true);
+		buildScreeningDetailTemplate(fldstEnt);
+
+		FieldSet fldstOther = new FieldSet();
+		templateSettings3.add(fldstOther, new FormData("100%"));
+		fldstOther.setLayout(new FormLayout());
+		fldstOther.setHeading("Other");
+		fldstOther.setCollapsible(true);
+		buildScreeningDetailTemplate(fldstOther);
 		
 		mainTemplateSettingsPanel.add(templateSettings1);
 		mainTemplateSettingsPanel.add(templateSettings2);
-		mainTemplateSettingsPanel.setHeight("200");
-		
+		mainTemplateSettingsPanel.add(templateSettings3);
+		mainTemplateSettingsPanel.setSize("700", "400");
 		lcTemplateComponent_1.add(mainTemplateSettingsPanel);
 		return lcTemplateComponent_1;
+	}
+
+	private void buildScreeningDetailTemplate(FieldSet fieldSet)
+	{
+
+		NumberField staartColumn = new NumberField();
+		staartColumn.setMaxLength(2);
+		staartColumn.setFieldLabel("Start Column");
+		fieldSet.add(staartColumn, new FormData("100%"));
+
+		NumberField endColumn = new NumberField();
+		endColumn.setMaxLength(2);
+		endColumn.setFieldLabel("End Column");
+		fieldSet.add(endColumn, new FormData("100%"));
+
 	}
 
 	private LayoutContainer buildUploadComponent()
