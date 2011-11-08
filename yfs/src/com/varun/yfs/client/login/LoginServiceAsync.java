6e8bc4616de3adb94ba1a9cd31a97e2b41848223
varun.tayur@gmail.com <varun.tayur@gmail.com@63eca3cb-c5b2-5fb5-8018-6bcedb7b94c6>
2011-11-08 11:17:35 +0000

    Introduction of Login and session management

--- yfs/src/com/varun/yfs/client/login/LoginServiceAsync.java
+++ yfs/src/com/varun/yfs/client/login/LoginServiceAsync.java
@@ -1,6 +1,15 @@
 package com.varun.yfs.client.login;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.varun.yfs.dto.UserDTO;
+
 public interface LoginServiceAsync
 {
+
+	void loginServer(String name, String password, AsyncCallback<UserDTO> callback);
+
+	void loginFromSessionServer(AsyncCallback<UserDTO> callback);
+
+	void logout(AsyncCallback<Void> callback);
 
 }
