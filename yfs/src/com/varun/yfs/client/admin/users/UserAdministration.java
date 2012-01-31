611862bb00026dae89c3571958751f2082c62bf3
varun.tayur@gmail.com <varun.tayur@gmail.com@63eca3cb-c5b2-5fb5-8018-6bcedb7b94c6>
2012-01-31 16:00:37 +0000

    Role Admin introduced

--- yfs/src/com/varun/yfs/client/admin/users/UserAdministration.java
+++ yfs/src/com/varun/yfs/client/admin/users/UserAdministration.java
@@ -12,6 +12,7 @@ import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.MessageBoxEvent;
 import com.extjs.gxt.ui.client.event.SelectionChangedEvent;
+import com.extjs.gxt.ui.client.event.SelectionChangedListener;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.CheckBoxListView;
@@ -19,8 +20,10 @@ import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
 import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.ComboBox.TriggerAction;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
 import com.extjs.gxt.ui.client.widget.form.SimpleComboBox;
+import com.extjs.gxt.ui.client.widget.form.SimpleComboValue;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
@@ -43,6 +46,7 @@ import com.varun.yfs.client.images.YfsImageBundle;
 import com.varun.yfs.dto.ChapterNameDTO;
 import com.varun.yfs.dto.ProjectDTO;
 import com.varun.yfs.dto.UserDTO;
+import java.util.Collections;
 
 public class UserAdministration extends LayoutContainer
 {
@@ -54,8 +58,6 @@ public class UserAdministration extends LayoutContainer
 	private final TextField<String> txtfldUsrName = new TextField<String>();
 	private final TextField<String> txtfldPassword = new TextField<String>();
 	private SimpleComboBox<String> userRole = new SimpleComboBox<String>();
-	private final CheckBoxListView<ChapterNameDTO> lstfldChapterNames = new CheckBoxListView<ChapterNameDTO>();
-	private final CheckBoxListView<ProjectDTO> lstfldProjects = new CheckBoxListView<ProjectDTO>();
 
 	private String curAdminEntity = "Default";
 	private ModelData currentModelData = new BaseModelData();
@@ -63,7 +65,7 @@ public class UserAdministration extends LayoutContainer
 
 	public UserAdministration()
 	{
-		setHeight("700");
+		setHeight("700px");
 	}
 
 	final Listener<MessageBoxEvent> l = new Listener<MessageBoxEvent>()
@@ -92,13 +94,13 @@ public class UserAdministration extends LayoutContainer
 		td_gridPanel.setRowspan(2);
 		mainPanel.add(gridPanel, td_gridPanel);
 		mainPanel.setScrollMode(Scroll.AUTOY);
-		mainPanel.setHeight("700");
+		mainPanel.setSize("635px", "700");
 
 		add(mainPanel, new FitData(5));
 
 		gridPanel.setLayout(new FitLayout());
 		gridPanel.setHeading(curAdminEntity);
-		gridPanel.setSize("250px", "550px");
+		gridPanel.setSize("300px", "550px");
 
 		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
 		ColumnConfig clmncnfgNewColumn = new ColumnConfig("name", "Name", 150);
@@ -116,52 +118,46 @@ public class UserAdministration extends LayoutContainer
 		editorGrid.setClicksToEdit(EditorGrid.ClicksToEdit.ONE);
 		gridPanel.add(editorGrid);
 
-		editorGrid.getSelectionModel().addListener(Events.SelectionChange, new Listener<SelectionChangedEvent<ModelData>>()
-		{
-			@SuppressWarnings("unchecked")
-			public void handleEvent(SelectionChangedEvent<ModelData> be)
-			{
-				List<ModelData> selection = be.getSelection();
-				if (selection.size() > 0)
+		editorGrid.getSelectionModel().addListener(Events.SelectionChange,
+				new Listener<SelectionChangedEvent<ModelData>>()
 				{
-					txtfldUsrName.clear();
-					txtfldPassword.clear();
-					lstfldChapterNames.getStore().removeAll();
-					lstfldProjects.getStore().removeAll();
-
-					ModelData modelData = selection.get(0);
-					txtfldUsrName.setValue(modelData.get("name").toString());
-					txtfldPassword.setValue(modelData.get("password").toString());
-					lstfldChapterNames.getStore().add((List<ChapterNameDTO>) currentModelData.get("lstChapterNames"));
-					lstfldProjects.getStore().add((List<ProjectDTO>) currentModelData.get("lstProjects"));
-					Object role = modelData.get("role");
-					if (role != null)
-						userRole.findModel(role.toString());
-
-					updateSelections((List<ModelData>) modelData.get("chapterNames"), lstfldChapterNames);
-					updateSelections((List<ModelData>) modelData.get("projects"), lstfldProjects);
-
-					lstfldChapterNames.refresh();
-					lstfldProjects.refresh();
-
-					userDetailsViewHolder.setVisible(true);
-					userDetailsViewHolder.focus();
-				}
-			}
-
-			@SuppressWarnings({ "rawtypes", "unchecked" })
-			private <E> void updateSelections(List<? extends ModelData> modelData, CheckBoxListView view)
-			{
-				for (ModelData modelData1 : modelData)
-				{
-					int idx = view.getStore().getModels().indexOf(modelData1);
-					if (idx >= 0)
+					@SuppressWarnings("unchecked")
+					public void handleEvent(SelectionChangedEvent<ModelData> be)
 					{
-						view.setChecked(view.getStore().getAt(idx), true);
+						List<ModelData> selection = be.getSelection();
+						if (selection.size() > 0)
+						{
+							txtfldUsrName.clear();
+							txtfldPassword.clear();
+							userRole.clearSelections();
+
+							ModelData modelData = selection.get(0);
+							txtfldUsrName.setValue(modelData.get("name").toString());
+							txtfldPassword.setValue(modelData.get("password").toString());
+							Object role = modelData.get("role");
+							if (role != null && userRole.findModel(role.toString()) != null)
+							{
+								userRole.setValue(userRole.findModel(role.toString()));
+							}
+
+							userDetailsViewHolder.setVisible(true);
+							userDetailsViewHolder.focus();
+						}
 					}
-				}
-			}
-		});
+
+					@SuppressWarnings({ "rawtypes", "unchecked" })
+					private <E> void updateSelections(List<? extends ModelData> modelData, CheckBoxListView view)
+					{
+						for (ModelData modelData1 : modelData)
+						{
+							int idx = view.getStore().getModels().indexOf(modelData1);
+							if (idx >= 0)
+							{
+								view.setChecked(view.getStore().getAt(idx), true);
+							}
+						}
+					}
+				});
 
 		ToolBar toolBar = new ToolBar();
 		mainPanel.setTopComponent(toolBar);
@@ -176,11 +172,6 @@ public class UserAdministration extends LayoutContainer
 
 				txtfldUsrName.clear();
 				txtfldPassword.clear();
-				lstfldChapterNames.getStore().getModels().clear();
-				lstfldProjects.getStore().getModels().clear();
-
-				lstfldChapterNames.refresh();
-				lstfldProjects.refresh();
 
 				userDetailsViewHolder.setVisible(true);
 				userDetailsViewHolder.focus();
@@ -232,32 +223,57 @@ public class UserAdministration extends LayoutContainer
 		frmpanelUserBasic.add(txtfldPassword, new FormData("80%"));
 
 		frmpanelUserBasic.add(userRole, new FormData("80%"));
+		userRole.setTriggerAction(TriggerAction.ALL);
 		userRole.setFieldLabel("Role");
 		userRole.add("Administrator");
 		userRole.add("Administrator - Chapter");
 		userRole.add("Area Co-Ordinator");
 
+		userRole.addSelectionChangedListener(new SelectionChangedListener<SimpleComboValue<String>>()
+		{
+			@Override
+			public void selectionChanged(SelectionChangedEvent<SimpleComboValue<String>> se)
+			{
+				if (se.getSelectedItem().equals("Administrator"))
+				{
+
+				} else if (se.getSelectedItem().equals("Administrator - Chapter"))
+				{
+
+				} else if (se.getSelectedItem().equals("Area Co-Ordinator"))
+				{
+				}
+
+			}
+		});
+
+		LayoutContainer layoutContainer = new LayoutContainer();
+		List<ColumnConfig> configs_1 = new ArrayList<ColumnConfig>();
+
+		ColumnConfig clmncnfgNewColumn_1 = new ColumnConfig("id", "Chapter/Project", 120);
+		configs_1.add(clmncnfgNewColumn_1);
+
+		ColumnConfig clmncnfgNewColumn_2 = new ColumnConfig("id", "Read", 50);
+		configs_1.add(clmncnfgNewColumn_2);
+
+		ColumnConfig clmncnfgNewColumn_3 = new ColumnConfig("id", "Write", 50);
+		configs_1.add(clmncnfgNewColumn_3);
+
+		ColumnConfig clmncnfgNewColumn_4 = new ColumnConfig("id", "Delete", 50);
+		configs_1.add(clmncnfgNewColumn_4);
+
+		EditorGrid<ModelData> editorGrid_1 = new EditorGrid<ModelData>(new ListStore<ModelData>(), new ColumnModel(
+				configs_1));
+		editorGrid_1.setStripeRows(true);
+		editorGrid_1.setLoadMask(true);
+		editorGrid_1.setColumnLines(true);
+		layoutContainer.add(editorGrid_1);
+		editorGrid_1.setHeight("300");
+		editorGrid_1.setBorders(true);
+		layoutContainer.setBorders(true);
+
 		userDetailsViewHolder.add(frmpanelUserBasic, new FitData(5));
-		lstfldChapterNames.setBorders(true);
-
-		lstfldChapterNames.setStore(new ListStore<ChapterNameDTO>());
-		lstfldChapterNames.setDisplayProperty("chapterName");
-		lstfldChapterNames.setHeight("150px");
-		lstfldChapterNames.setStyleAttribute("overflow-y", "scroll");
-		final ContentPanel cPanelChapterNames = new ContentPanel();
-		cPanelChapterNames.setHeading("Select Chapters");
-		cPanelChapterNames.add(lstfldChapterNames);
-
-		userDetailsViewHolder.add(cPanelChapterNames, new FitData(5));
-
-		lstfldProjects.setStore(new ListStore<ProjectDTO>());
-		lstfldProjects.setHeight("150px");
-		lstfldProjects.setDisplayProperty("projectName");
-		lstfldProjects.setStyleAttribute("overflow-y", "scroll");
-		final ContentPanel cPanelProjects = new ContentPanel();
-		cPanelProjects.setHeading("Select Projects");
-		cPanelProjects.add(lstfldProjects);
-		userDetailsViewHolder.add(cPanelProjects, new FitData(5));
+		userDetailsViewHolder.add(layoutContainer, new FitData(5));
 
 		TableData td_lstViewHolder = new TableData();
 		td_lstViewHolder.setHorizontalAlign(HorizontalAlignment.LEFT);
@@ -292,8 +308,6 @@ public class UserAdministration extends LayoutContainer
 					models.add(modelData);
 					modelData.setName(txtfldUsrName.getValue());
 					modelData.setPassword(txtfldPassword.getValue());
-					modelData.setChapterNames(lstfldChapterNames.getChecked());
-					modelData.setProjects(lstfldProjects.getChecked());
 					modelData.setRole(userRole.getSimpleValue());
 				} else
 				{
@@ -302,8 +316,6 @@ public class UserAdministration extends LayoutContainer
 					{
 						modelData.setName(txtfldUsrName.getValue());
 						modelData.setPassword(txtfldPassword.getValue());
-						modelData.setChapterNames(lstfldChapterNames.getChecked());
-						modelData.setProjects(lstfldProjects.getChecked());
 						modelData.setRole(userRole.getSimpleValue());
 					}
 				}
